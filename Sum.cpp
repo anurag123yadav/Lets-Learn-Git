206e504b2e214fcd1310c73b64118c64eb42e198
Anurag Yadav <anurag.ay123@gmail.com>
2023-06-02 17:48:08 +0530

    intial commit

---
+++ Sum.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int sum(int a, int b){
+  return a+b;
+}
+int main(){
+  int a=5;
+  int b=7;
+  int n =8; 
+  sum(a,b);
+  cout<<sum(5,7);
+  for (int i=0; i <n; i++){
+    cout<<i;
+  }
+}
